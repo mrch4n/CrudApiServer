26c9e021bbf96a2dc2a2903274395ed573508dc8
HenryChan <cwhmobi@gmail.com>
2021-08-31 20:09:38 +0800

    Improved: better api error message Updated: api "api/availability/:pid" GET method handler.

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,13 +5,13 @@ function apiRoute(app) {
     .get(product.findAll)
     .post(product.create);
 
-  app.route('/api/products/:id')
+  app.route('/api/products/:id?')
     .get(product.findById)
     .put(product.updateById)
     .delete(product.deleteById);
 
-  app.route('/api/availability/:pid')
-    .get(product.checkAvailability)
+  app.route('/api/availability/:pid?')
+    .get(product.getAvailabilities)
     .put(product.setAvailability);
   app.route('/api/available/:pid?')
     .get(product.checkAvailability);
